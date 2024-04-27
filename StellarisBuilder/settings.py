42ca6add102bd9e55c28c29e52d0cd3f5d16bdcf
musfobbie@mail.ru <musfobbie@mail.ru>
2024-04-27 16:59:45 +0300

    add psycopg2, link PostgreSQL

--- StellarisBuilder/settings.py
+++ StellarisBuilder/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'StellarisBuilder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'StellarisBuilderSite',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
