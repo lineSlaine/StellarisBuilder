cb590e0bb0a02fc12dd1506452e145eae71c2337
musfobbie@mail.ru <musfobbie@mail.ru>
2024-04-30 23:33:20 +0300

    set method names

--- apps/StellarisSave/views.py
+++ apps/StellarisSave/views.py
@@ -8,9 +8,10 @@ from apps.StellarisSave.serializers import EthicSerializer
 # # Create your views here.
 
 class EthicView(viewsets.ModelViewSet):
-
+    http_method_names = ['get']
     serializer_class = EthicSerializer
     queryset = Ethic.objects.all()
+
     # def get_queryset(self):
     #     user = self.request.user
     #     return SaveListStellaris.objects.filter(user=user.id)
